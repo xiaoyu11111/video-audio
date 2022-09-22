c28a364bd27305ef681de51d8230c45410b9de78
xiaoyu111111 <1848051540@qq.com>
2022-09-22 18:20:04 +0800

    fix: mp3转wav

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ const debug = process.env.NODE_ENV !== 'production'
 module.exports = {
     publicPath: './', //vueConf.baseUrl, // 根域上下文目录
     outputDir: 'dist', // 构建输出目录
-    assetsDir: 'assets', // 静态资源目录 (js, css, img, fonts)
+    assetsDir: 'animation-editor-assets', // 静态资源目录 (js, css, img, fonts)
     lintOnSave: false, // 是否开启eslint保存检测，有效值：ture | false | 'error'
     runtimeCompiler: true, // 运行时版本是否需要编译
     transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
